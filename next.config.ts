5e3692d873d255daedcd7014706d64a14a9fbe85
Willi Hyde <976951+WilliHyde@users.noreply.github.com>
2025-01-22 22:45:01 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: isProd ? "/japanese-kanji-cards" : "",
+  basePath: isProd ? "/japanese-kanji-cards" : "",
+  output: "export"
 };
 
 export default nextConfig;
